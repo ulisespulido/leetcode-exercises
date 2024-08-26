437dd9abc3039a0d90ee38f120217a078231b82e
Ulises Pulido <ulises@sawyereffect.com>
2024-08-26 17:15:47 -0400

    sudoku v2

--- easy/arrays/valid-sudoku.js
+++ easy/arrays/valid-sudoku.js
@@ -1,11 +1,3 @@
-
-/**
- * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
- * Each row must contain the digits 1-9 without repetition.
- * Each column must contain the digits 1-9 without repetition.
- * Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
- */
-
 /**
  * @param {character[][]} board
  * @return {boolean}
@@ -21,30 +13,18 @@ var isValidSudoku = function(board) {
                 continue;
             }
             rows[i] = rows[i] || {};
-            if (rows[i][num]) {
-                return false;
-            } else {
-                rows[i][num] = true;
-            }
-            
             columns[j] = columns[j] || {};
-            if(columns[j][num]) {
-                return false;
-            } else {
-                columns[j][num] = true;
-            }
             let squarePosition = `${Math.floor(i/3)}${Math.floor(j/3)}`;
             squares[squarePosition] = squares[squarePosition] || {};
-            if (squares[squarePosition][num]) {
+            
+            if (rows[i][num] || columns[j][num] || squares[squarePosition][num]) {
                 return false;
-            } else {
-                squares[squarePosition][num] = true;
-            }
+            } 
+            rows[i][num] = true;
+            columns[j][num] = true;
+            squares[squarePosition][num] = true;
         }
     }
     
     return true;
 };
-
-// O(9) time
-// O(9*9*3) space
